b39a8e30a8831ddc7b88edcbcdacdbdf98e00593
Tyler Yeary <teyeary@fullsail.edu>
2017-02-14 16:46:56 -0500

    Created basic structure to register users and login users. More work to be done.

---
+++ Victorious/WebApplication/Controllers/AccountController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Account/Login")]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("Account/Login")]
+        public ActionResult Login(LoginViewModel user)
+        {
+            String adminName = "Admin";
+            String adminPass = "Pass";
+
+            if (ModelState.IsValid)
+            {
+                //We need now to check the login credentias of the user
+
+                //Query for the username
+                if (user.Username == adminName)
+                {
+                    //Verfy the password is correct
+                    if (user.Password == adminPass)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "This is an invalid model");
+            }
+
+            return View(user);
+        }
+        
+        [Route("Account/Register")]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("Account/Register")]
+        public ActionResult Register(RegisterViewModel user)
+        {
+
+            //If we hit this, then something failed 
+            return View(user);
+        }
+
+        [Route("Account/Logout")]
+        public ActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
---
+++ Victorious/WebApplication/Models/AccountViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name="UserName")]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name ="Password")]
+        public string Password { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "UserName")]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+}
