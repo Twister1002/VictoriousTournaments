1a54bb52bb454ff8fac953a217e45a9eb072cd5a
Sean Davis <seandavis@fullsail.edu>
2017-02-23 14:26:19 -0500

    DoubleElimBracket TESTED. Still needs updating for abnormal bracket sizes, and intensive re-seeding.

--- Victorious/Tournament.Structure.Tests/TournamentStructureTests.cs
+++ Victorious/Tournament.Structure.Tests/TournamentStructureTests.cs
@@ -425,5 +425,136 @@ namespace Tournament.Structure.Tests
 			Assert.AreEqual(playerNum, b.Rounds[0][0].PlayerIndexes[1]);
 		}
 		#endregion
+
+		#region DoubleElimBracket Tests
+		[TestMethod]
+		[TestCategory("DoubleElimBracket")]
+		[TestCategory("DoubleElimBracket Ctor")]
+		public void DEBCtor_Constructs()
+		{
+			IBracket b = new DoubleElimBracket();
+
+			Assert.IsInstanceOfType(b, typeof(DoubleElimBracket));
+		}
+		[TestMethod]
+		[TestCategory("DoubleElimBracket")]
+		[TestCategory("DoubleElimBracket Ctor")]
+		public void DEB_InheritsFieldsFromSEB()
+		{
+			IBracket b = new DoubleElimBracket();
+
+			Assert.AreEqual(0, b.Players.Count);
+		}
+
+		[TestMethod]
+		[TestCategory("DoubleElimBracket")]
+		[TestCategory("DoubleElimBracket Methods")]
+		public void DEB_CreateBracket_InheritsUpperBracketCorrectly()
+		{
+			List<IPlayer> pList = new List<IPlayer>();
+			for (int i = 0; i < 8; ++i)
+			{
+				pList.Add(new Mock<IPlayer>().Object);
+			}
+			IBracket b = new DoubleElimBracket(pList);
+			b.CreateBracket(2);
+
+			Assert.AreEqual(3, b.Rounds.Count);
+		}
+		[TestMethod]
+		[TestCategory("DoubleElimBracket")]
+		[TestCategory("DoubleElimBracket Methods")]
+		public void DEB_CreateBracket_CreatesLowerBracket()
+		{
+			List<IPlayer> pList = new List<IPlayer>();
+			for (int i = 0; i < 8; ++i)
+			{
+				pList.Add(new Mock<IPlayer>().Object);
+			}
+			IBracket b = new DoubleElimBracket(pList);
+			b.CreateBracket(2);
+
+			int numLowerRounds = (b is DoubleElimBracket) ? (b as DoubleElimBracket).LowerRounds.Count : 0;
+
+			Assert.AreEqual(4, numLowerRounds);
+		}
+		[TestMethod]
+		[TestCategory("DoubleElimBracket")]
+		[TestCategory("DoubleElimBracket Methods")]
+		public void DEB_CreateBracket_CreatesLowerBracketMatches()
+		{
+			List<IPlayer> pList = new List<IPlayer>();
+			for (int i = 0; i < 8; ++i)
+			{
+				pList.Add(new Mock<IPlayer>().Object);
+			}
+			IBracket b = new DoubleElimBracket(pList);
+			b.CreateBracket(2);
+
+			int numFirstRoundMatches = (b is DoubleElimBracket)
+				? (b as DoubleElimBracket).LowerRounds[(b as DoubleElimBracket).LowerRounds.Count - 1].Count
+				: 0;
+
+			Assert.AreEqual(2, numFirstRoundMatches);
+		}
+		[TestMethod]
+		[TestCategory("DoubleElimBracket")]
+		[TestCategory("DoubleElimBracket Methods")]
+		public void DEB_AddWin_MovesLoserToLowerBracket()
+		{
+			List<IPlayer> pList = new List<IPlayer>();
+			for (int i = 0; i < 8; ++i)
+			{
+				pList.Add(new Mock<IPlayer>().Object);
+			}
+			IBracket b = new DoubleElimBracket(pList);
+			b.CreateBracket();
+
+			int pIndex = b.Rounds[2][1].PlayerIndexes[0];
+			b.AddWin(b.Rounds[2][1], 1);
+
+			Assert.AreEqual(pIndex, (b as DoubleElimBracket).LowerRounds[3][0].PlayerIndexes[1]);
+		}
+		[TestMethod]
+		[TestCategory("DoubleElimBracket")]
+		[TestCategory("DoubleElimBracket Methods")]
+		public void DEB_AddWin_MovesTeamsThroughLowerBracket()
+		{
+			List<IPlayer> pList = new List<IPlayer>();
+			for (int i = 0; i < 8; ++i)
+			{
+				pList.Add(new Mock<IPlayer>().Object);
+			}
+			IBracket b = new DoubleElimBracket(pList);
+			b.CreateBracket();
+			
+			b.AddWin(b.Rounds[2][0], 0);
+			b.AddWin(b.Rounds[2][1], 0);
+			int pIndex = (b is DoubleElimBracket) ? (b as DoubleElimBracket).LowerRounds[3][0].PlayerIndexes[0] : 0;
+			b.AddWin((b as DoubleElimBracket).LowerRounds[3][0], 0);
+
+			Assert.AreEqual(pIndex, (b as DoubleElimBracket).LowerRounds[2][0].PlayerIndexes[0]);
+		}
+		[TestMethod]
+		[TestCategory("DoubleElimBracket")]
+		[TestCategory("DoubleElimBracket Methods")]
+		public void DEB_AddWin_DropsLoserFromSecondRound()
+		{
+			List<IPlayer> pList = new List<IPlayer>();
+			for (int i = 0; i < 4; ++i)
+			{
+				pList.Add(new Mock<IPlayer>().Object);
+			}
+			IBracket b = new DoubleElimBracket(pList);
+			b.CreateBracket();
+
+			b.AddWin(b.Rounds[1][0], 0);
+			b.AddWin(b.Rounds[1][1], 0);
+			int pIndex = b.Rounds[0][0].PlayerIndexes[1];
+			b.AddWin(b.Rounds[0][0], 0);
+
+			Assert.AreEqual(pIndex, (b as DoubleElimBracket).LowerRounds[0][0].PlayerIndexes[0]);
+		}
+		#endregion
 	}
 }
--- Victorious/Tournament.Structure/DoubleElimBracket.cs
+++ Victorious/Tournament.Structure/DoubleElimBracket.cs
@@ -25,13 +25,13 @@ namespace Tournament.Structure
 		{
 			base.CreateBracket(_winsPerMatch);
 			LowerRounds.Clear();
-			
+
 			int totalMatches = Players.Count - 2;
 			int numMatches = 0;
 			int roundIndex = 0;
 			while (numMatches < totalMatches)
 			{
-				Rounds.Add(new List<IMatch>());
+				LowerRounds.Add(new List<IMatch>());
 				for (int i = 0;
 					i < Math.Pow(2, roundIndex / 2) && numMatches < totalMatches;
 					++i, ++numMatches)
@@ -40,14 +40,15 @@ namespace Tournament.Structure
 					// (rounds[0] is the final match)
 					IMatch m = new Match();
 					m.RoundNumber = roundIndex;
-					m.MatchIndex = Rounds[roundIndex].Count;
+					m.MatchIndex = LowerRounds[roundIndex].Count;
 					m.WinsNeeded = _winsPerMatch;
-					Rounds[roundIndex].Add(m);
+					LowerRounds[roundIndex].Add(m);
 				}
 				++roundIndex;
 			}
 
-			for (int rIndex = 0; rIndex + 1 < Rounds.Count; ++rIndex)
+			int rIndex = 0;
+			for (; rIndex + 1 < LowerRounds.Count; ++rIndex)
 			{
 				bool rIndexIsEven = (0 == rIndex % 2) ? true : false;
 				if (rIndexIsEven && LowerRounds[rIndex + 1].Count == LowerRounds[rIndex].Count)
@@ -61,7 +62,7 @@ namespace Tournament.Structure
 						// *************** THIS ISN'T QUITE RIGHT [mIndex]
 
 						LowerRounds[rIndex][mIndex].AddPrevMatchIndex(mIndex * 2);
-						LowerRounds[rIndex + 1][mIndex * 2].NextMatchIndex = mIndex;
+						LowerRounds[rIndex + 1][mIndex].NextMatchIndex = mIndex;
 					}
 				}
 				else if (!rIndexIsEven && LowerRounds[rIndex + 1].Count == (LowerRounds[rIndex].Count * 2))
@@ -81,17 +82,84 @@ namespace Tournament.Structure
 				{
 					// Round is abnormal. FUCK. Do something???
 				}
+			}
 
-				rIndex = LowerRounds.Count - 1;
-				for (int mIndex = 0; mIndex < LowerRounds[rIndex].Count; ++mIndex)
+			rIndex = LowerRounds.Count - 1;
+			for (int mIndex = 0; mIndex < LowerRounds[rIndex].Count; ++mIndex)
+			{
+				// Assign prev/next matchup indexes for FIRST round
+				// (both teams come from Upper Bracket)
+				LowerRounds[rIndex][mIndex].AddPrevMatchIndex(mIndex * -2);
+				Rounds[Rounds.Count - 1][mIndex * 2].NextLoserMatchIndex = mIndex;
+
+				LowerRounds[rIndex][mIndex].AddPrevMatchIndex(mIndex * -2 - 1);
+				Rounds[Rounds.Count - 1][mIndex * 2 + 1].NextLoserMatchIndex = mIndex;
+			}
+		}
+
+		public override void AddWin(IMatch _match, int _index)
+		{
+			for (int r = 0; r < Rounds.Count; ++r)
+			{
+				for (int m = 0; m < Rounds[r].Count; ++m)
 				{
-					// Assign prev/next matchup indexes for FIRST round
-					// (both teams come from Upper Bracket)
-					LowerRounds[rIndex][mIndex].AddPrevMatchIndex(mIndex * -2);
-					Rounds[Rounds.Count - 1][mIndex * 2].NextLoserMatchIndex = mIndex;
+					if (Rounds[r][m] == _match)
+					{
+						base.AddWin(r, m, _index);
 
-					LowerRounds[rIndex][mIndex].AddPrevMatchIndex(mIndex * -2 - 1);
-					Rounds[Rounds.Count - 1][mIndex * 2 + 1].NextLoserMatchIndex = mIndex;
+						if (Rounds[r][m].Score[_index] >= Rounds[r][m].WinsNeeded)
+						{
+							// Match over: move the loser
+							int loserIndex = (0 == _index) ? 1 : 0;
+							int nMatchIndex = Rounds[r][m].NextLoserMatchIndex;
+							int nRoundIndex = (r == Rounds.Count - 1) ? (r * 2 - 1) : (r * 2);
+							for (int i = 0; i < LowerRounds[nRoundIndex][nMatchIndex].PrevMatchIndexes.Count; ++i)
+							{
+								if (m == (-1 * LowerRounds[nRoundIndex][nMatchIndex].PrevMatchIndexes[i]))
+								{
+									LowerRounds[nRoundIndex][nMatchIndex].PlayerIndexes[i] = Rounds[r][m].PlayerIndexes[loserIndex];
+									return;
+								}
+							}
+						}
+					}
+				}
+			}
+
+			// If we reach here, _match is not in the Upper Bracket
+			for (int r = 0; r < LowerRounds.Count; ++r)
+			{
+				for (int m = 0; m < LowerRounds[r].Count; ++m)
+				{
+					if (_match == LowerRounds[r][m])
+					{
+						// Found match. Add win:
+						if (false == LowerRounds[r][m].AddWin(_index))
+						{
+							return;
+						}
+
+						if (LowerRounds[r][m].Score[_index] >= LowerRounds[r][m].WinsNeeded)
+						{
+							// Match is over. Move the winner forward:
+							if (0 == r)
+							{
+								// TODO : Move winner to Grand Finals
+								return;
+							}
+
+							// Move the winner forward
+							int nmIndex = LowerRounds[r][m].NextMatchIndex;
+							for (int i = 0; i < LowerRounds[r - 1][nmIndex].PrevMatchIndexes.Count; ++i)
+							{
+								if (m == LowerRounds[r - 1][nmIndex].PrevMatchIndexes[i])
+								{
+									LowerRounds[r - 1][nmIndex].PlayerIndexes[i] = LowerRounds[r][m].PlayerIndexes[_index];
+									return;
+								}
+							}
+						}
+					}
 				}
 			}
 		}
