ab028dd5dadd2878081e1793c0dc90182091a864
Tyler Yeary <teyeary@fullsail.edu>
2017-03-01 19:16:49 -0500

    Made styling changes to help with placements on bracket displaying.

--- Victorious/WebApplication/Models/TournamentViewModel.cs
+++ Victorious/WebApplication/Models/TournamentViewModel.cs
@@ -45,7 +45,7 @@ namespace WebApplication.Models
         public TournamentViewModel()
         {
             tourny = new Tournament.Structure.Tournament();
-            for (int i = 0; i < 30; i++)
+            for (int i = 1; i <= 8; i++)
             {
                 UserModel uModel = new UserModel()
                 {
