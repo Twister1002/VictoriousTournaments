997f7e4180a898289a1949cd183606e60d66acc5
Tyler Yeary <teyeary@fullsail.edu>
2017-03-16 01:03:52 -0400

    Attempted to do UnitTests.... Failed miserably.

---
+++ Victorious/WebApplication.Tests/Controllers/AccountControllerUnit.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication.Controllers;
+
+namespace WebApplication.Tests.Controllers
+{
+    [TestClass]
+    public class AccountControllerUnit
+    {
+        [TestMethod]
+        public void AccountController_Logout_DestroysAllSessions()
+        {
+            // Arrange
+            AccountController controller = new AccountController();
+
+            // Act
+            
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void AccountController_Index_Returns_LoginRedirect()
+        {
+            // Arrange
+            AccountController controller = new AccountController();
+
+            // Act
+
+
+            // Assert
+        }
+    }
+}
