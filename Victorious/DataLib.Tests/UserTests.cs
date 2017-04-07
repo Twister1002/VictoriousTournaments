9ab94351101be46a1ea7a47670bfdee6c1984900
Ryan <keltonr01@gmail.com>
2017-04-07 12:19:28 -0400

    Added GameTypeModel

--- Victorious/DataLib.Tests/UserTests.cs
+++ Victorious/DataLib.Tests/UserTests.cs
@@ -1,17 +1,56 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataLib;
+using System.Data.Entity;
 
 namespace DataLib.Tests
 {
     [TestClass]
     public class UserTests
     {
-        
+        //private VictoriousDbContext context;
+
+        [ClassInitialize]
+        void TestSetup()
+        {
+            //context = new VictoriousDbContext();
+            //if (context.Database.Exists())
+            //{
+            //    context.Database.Delete();
+            //}
+            //context.Database.Create();
+        }
 
         [TestMethod]
-        public void AddUser()
+        public void Test_UsernameExists()
         {
+            var db = new DatabaseInterface();
             
+        }
+
+        [TestMethod]
+        public void Test_AddUser_and_UsernameExists()
+        {
+            var dbInterface = new DatabaseInterface();
+            var user = new UserModel()
+            {
+                FirstName = "Ryan",
+                LastName = "Kelton",
+                Username = "keltonr01",
+                Password = "1234",
+                SitePermission = new PermissionModel()
+                {
+                    Permission = Permission.SITE_ADMINISTRATOR
+                }
+            };
+            if (dbInterface.UserUsernameExists(user.Username) == DbError.DOES_NOT_EXIST)
+            {
+                user.Username = "keltonr02";
+                dbInterface.AddUser(user); 
+            }
+
+            var result = dbInterface.GetUserByUsername("keltonr01");
+            Assert.AreEqual(user, result);
         }
     }
 }
