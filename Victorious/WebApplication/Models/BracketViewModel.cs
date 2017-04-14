7d1c743877e51df1aac56ee56063eef457e1675b
Tyler Yeary <teyeary@fullsail.edu>
2017-04-14 18:43:53 -0400

    Restructuring the entire tournament display. Created BracketViewModel to host the IBracket and to get the bracket from database.

---
+++ Victorious/WebApplication/Models/BracketViewModel.cs
@@ -0,0 +1,25 @@
+﻿using DataLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Tournament.Structure;
+
+namespace WebApplication.Models
+{
+    public class BracketViewModel : ViewModel
+    {
+        public IBracket Bracket { get; private set; }
+        public BracketModel Model { get; private set; }
+
+        public BracketViewModel()
+        {
+
+        }
+
+        public BracketViewModel(IBracket bracket)
+        {
+            Bracket = bracket;
+        }
+    }
+}
