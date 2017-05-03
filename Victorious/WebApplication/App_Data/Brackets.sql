8fd757c3fdd128738c7c5738a01650a18466b0cc
Tyler Yeary <teyeary@fullsail.edu>
2017-05-03 19:11:29 -0400

    Added SQL files.

---
+++ Victorious/WebApplication/App_Data/Brackets.sql
@@ -0,0 +1,3 @@
+﻿SET IDENTITY_INSERT [dbo].[Brackets] ON
+INSERT INTO [dbo].[Brackets] ([BracketID], [BracketTitle], [BracketTypeID], [Finalized], [NumberOfGroups], [TournamentID], [MaxRounds]) VALUES (2, N'Single', 1, 0, 0, 2, 3)
+SET IDENTITY_INSERT [dbo].[Brackets] OFF
