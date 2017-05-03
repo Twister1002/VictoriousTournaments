5f1a0a762ff274de7305aa6d141ca80ae7e4680c
Tyler Yeary <teyeary@fullsail.edu>
2017-05-02 20:43:41 -0400

    Fixed reference error

--- Victorious/WebApplication/Models/GameViewModel.cs
+++ Victorious/WebApplication/Models/GameViewModel.cs
@@ -1,9 +1,5 @@
-﻿using DataLib;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using Tournament.Structure;
+﻿using Tournament.Structure;
+using DatabaseLib;
 
 namespace WebApplication.Models
 {
