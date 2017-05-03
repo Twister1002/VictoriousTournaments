2348bd31297cc84ecabe5380cdb27e74c29c64a3
Tyler Yeary <teyeary@fullsail.edu>
2017-05-03 19:05:11 -0400

    Added SQL files.

---
+++ Victorious/WebApplication/App_Data/GameTypes.sql
@@ -0,0 +1,3 @@
+﻿SET IDENTITY_INSERT [dbo].[GameTypes] ON
+INSERT INTO [dbo].[GameTypes] ([GameTypeID], [Title]) VALUES (1, N'Test Game')
+SET IDENTITY_INSERT [dbo].[GameTypes] OFF
--- Victorious/WebApplication/App_Data/UsersToTournament.sql
+++ Victorious/WebApplication/App_Data/UsersToTournament.sql
@@ -1,18 +1,20 @@
 ﻿SET IDENTITY_INSERT [dbo].[TournamentUsers] ON
 INSERT INTO [dbo].[TournamentUsers] ([TournamentUserID], [AccountID], [Username], [UniformNumber], [TournamentID], [PermissionLevel], [Name]) VALUES 
-(99991, 101, N'Player1', NULL, 2, 102, N'Player1')
+(1, 1, N'test', NULL, 2, 102, N'test')
 INSERT INTO [dbo].[TournamentUsers] ([TournamentUserID], [AccountID], [Username], [UniformNumber], [TournamentID], [PermissionLevel], [Name]) VALUES 
-(99992, 102, N'Player2', NULL, 2, 102, N'Player2')
+(99991, 101, N'Player1', NULL, 2, 101, N'Player1')
 INSERT INTO [dbo].[TournamentUsers] ([TournamentUserID], [AccountID], [Username], [UniformNumber], [TournamentID], [PermissionLevel], [Name]) VALUES 
-(99993, 103, N'Player3', NULL, 2, 102, N'Player3')
+(99992, 102, N'Player2', NULL, 2, 101, N'Player2')
 INSERT INTO [dbo].[TournamentUsers] ([TournamentUserID], [AccountID], [Username], [UniformNumber], [TournamentID], [PermissionLevel], [Name]) VALUES 
-(99994, 104, N'Player4', NULL, 2, 102, N'Player4')
+(99993, 103, N'Player3', NULL, 2, 101, N'Player3')
 INSERT INTO [dbo].[TournamentUsers] ([TournamentUserID], [AccountID], [Username], [UniformNumber], [TournamentID], [PermissionLevel], [Name]) VALUES 
-(99995, 105, N'Player5', NULL, 2, 102, N'Player5')
+(99994, 104, N'Player4', NULL, 2, 101, N'Player4')
 INSERT INTO [dbo].[TournamentUsers] ([TournamentUserID], [AccountID], [Username], [UniformNumber], [TournamentID], [PermissionLevel], [Name]) VALUES 
-(99996, 106, N'Player6', NULL, 2, 102, N'Player6')
+(99995, 105, N'Player5', NULL, 2, 101, N'Player5')
 INSERT INTO [dbo].[TournamentUsers] ([TournamentUserID], [AccountID], [Username], [UniformNumber], [TournamentID], [PermissionLevel], [Name]) VALUES 
-(99997, 107, N'Player7', NULL, 2, 102, N'Player7')
+(99996, 106, N'Player6', NULL, 2, 101, N'Player6')
 INSERT INTO [dbo].[TournamentUsers] ([TournamentUserID], [AccountID], [Username], [UniformNumber], [TournamentID], [PermissionLevel], [Name]) VALUES 
-(99998, 108, N'Player8', NULL, 2, 102, N'Player8')
+(99997, 107, N'Player7', NULL, 2, 101, N'Player7')
+INSERT INTO [dbo].[TournamentUsers] ([TournamentUserID], [AccountID], [Username], [UniformNumber], [TournamentID], [PermissionLevel], [Name]) VALUES 
+(99998, 108, N'Player8', NULL, 2, 101, N'Player8')
 SET IDENTITY_INSERT [dbo].[TournamentUsers] OFF
