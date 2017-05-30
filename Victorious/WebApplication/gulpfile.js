5fc88ea03eac3621c682dd0fe89e2a60c723cc39
Tyler Yeary <teyeary@fullsail.edu>
2017-05-30 18:08:13 -0400

    Removed JQuery from resources in Visual Studio.

--- Victorious/WebApplication/gulpfile.js
+++ Victorious/WebApplication/gulpfile.js
@@ -1,4 +1,4 @@
-﻿/// <binding BeforeBuild='less, js' Clean='js-clean, less-clean' ProjectOpened='watch-js, watch-less' />
+/// <binding BeforeBuild='less, js' Clean='less-clean, js-clean' />
 /*
 This file in the main entry point for defining Gulp tasks and using Gulp plugins.
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
