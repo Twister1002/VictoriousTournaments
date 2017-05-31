46aa0c703d5209d7c06622cdbef1026370a6f518
Tyler Yeary <teyeary@fullsail.edu>
2017-05-31 12:21:05 -0400

    Fixed some issues with Gulp. Created the footer of the page.

--- Victorious/WebApplication/gulpfile.js
+++ Victorious/WebApplication/gulpfile.js
@@ -1,4 +1,4 @@
-/// <binding BeforeBuild='less, js' Clean='less-clean, js-clean' />
+/// <binding BeforeBuild='less, js' Clean='less-clean, js-clean' ProjectOpened='watch-js, watch-less' />
 /*
 This file in the main entry point for defining Gulp tasks and using Gulp plugins.
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
@@ -26,6 +26,10 @@ var gulp = require("gulp"),
         ]
     };
 
+function error(error) {
+    console.log(error.toString());
+    this.emit("end");
+}
 gulp.task("js-clean", function() {
     return del(["Scripts/scripts.min.js"]);
 });
@@ -36,15 +40,17 @@ gulp.task("less-clean", function () {
 
 gulp.task("js", ["js-clean"], function () {
     return gulp.src(config.js)
-    .pipe(uglify())
-    .pipe(concat("scripts.min.js"))
-    .pipe(gulp.dest("Scripts/"));
+        .pipe(uglify())
+        .on("error", error)
+        .pipe(concat("scripts.min.js"))
+        .pipe(gulp.dest("Scripts/"));
 });
 
 gulp.task("less", ["less-clean"], function () {
     return gulp.src(config.less)
         .pipe(concat("main.min.css"))
         .pipe(less())
+        .on("error", error)
         .pipe(gulp.dest("Styles/css/"));
 });
 
