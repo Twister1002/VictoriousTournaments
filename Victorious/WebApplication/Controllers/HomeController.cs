f9ed604ed4466cf67fab02db020a033df45fa495
Tyler Yeary <teyeary@fullsail.edu>
2017-05-31 16:53:38 -0400

    Added new pages to the site; needs some content.

--- Victorious/WebApplication/Controllers/HomeController.cs
+++ Victorious/WebApplication/Controllers/HomeController.cs
@@ -5,12 +5,48 @@ namespace WebApplication.Controllers
 {
     public class HomeController : VictoriousController
     {
-        // GET: Home
+        [Route("")]
         public ActionResult Index()
         {
             HomeViewModel model = new HomeViewModel();
 
-            return View(model);
+            return View("Index", model);
+        }
+
+        [Route("FAQ")]
+        public ActionResult FAQ()
+        {
+            HomeViewModel model = new HomeViewModel();
+            return View("FAQ", model);
+        }
+
+        [Route("Blog")]
+        public ActionResult Blog()
+        {
+            Session["Message"] = "The blog is currently not working.";
+            Session["Message.Class"] = ViewModel.ViewError.CRITICAL;
+            return RedirectToAction("Index");
+        }
+
+        [Route("About")]
+        public ActionResult About()
+        {
+            HomeViewModel model = new HomeViewModel();
+            return View("About", model);
+        }
+
+        [Route("Contact")]
+        public ActionResult Contact()
+        {
+            HomeViewModel model = new HomeViewModel();
+            return View("Contact", model);
+        }
+
+        [Route("Rules")]
+        public ActionResult Rules()
+        {
+            HomeViewModel model = new HomeViewModel();
+            return View("Rules", model);
         }
     }
 }
