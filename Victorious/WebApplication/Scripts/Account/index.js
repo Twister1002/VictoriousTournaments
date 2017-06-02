88d769adbf2e05776a09001c77132398ebb12372
Tyler Yeary <teyeary@fullsail.edu>
2017-06-02 13:41:24 -0400

    Created basic layout of new design.

---
+++ Victorious/WebApplication/Scripts/Account/index.js
@@ -0,0 +1,8 @@
+﻿jQuery(document).ready(function ($) {
+    
+    $(".accountMenu .menu").on("click", function () {
+        $(this).addClass("active").siblings().removeClass("active");
+        $("#Account" + $(this).data("type")).addClass("active").siblings().removeClass("active");
+    });
+
+});
