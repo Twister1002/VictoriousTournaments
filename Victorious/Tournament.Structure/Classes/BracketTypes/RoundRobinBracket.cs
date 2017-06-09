bb8a313686f06bfa571bf6099a5c77c0b30b8690
Sean Davis <seandavis@fullsail.edu>
2017-06-09 11:55:01 -0400

    Added ability to generate tie-breakers for round robins. (currently turned off)

--- Victorious/Tournament.Structure/Classes/BracketTypes/RoundRobinBracket.cs
+++ Victorious/Tournament.Structure/Classes/BracketTypes/RoundRobinBracket.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// #define ENABLE_TIEBREAKERS
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -181,7 +183,97 @@ namespace Tournament.Structure
 		protected override List<MatchModel> ApplyWinEffects(int _matchNumber, PlayerSlot _slot)
 		{
 			this.IsFinished = !(Matches.Values.Any(m => !m.IsFinished));
+#if ENABLE_TIEBREAKERS
+			// Check for, and create, Tiebreaker matches:
+			if (IsFinished)
+			{
+				// Calculate W/L scores for all players:
+				int[] scores = new int[Rankings.Count];
+				for (int i = 0; i < Rankings.Count; ++i)
+				{
+					scores[i] = Rankings[i].CalculateScore(MatchWinValue, MatchTieValue, 0);
+				}
 
+				// Create a "group" for any tied players:
+				List<List<int>> tiedGroups = new List<List<int>>();
+				for (int i = 0; i < Rankings.Count - 1; )
+				{
+					int j = i + 1;
+					for ( ; j < Rankings.Count; ++j)
+					{
+						if (scores[i] != scores[j])
+						{
+							break;
+						}
+
+						if (i + 1 == j)
+						{
+							tiedGroups.Add(new List<int>());
+							tiedGroups[tiedGroups.Count - 1].Add(Rankings[i].Id);
+						}
+						tiedGroups[tiedGroups.Count - 1].Add(Rankings[j].Id);
+					}
+					i = j;
+				}
+				if (0 == tiedGroups.Count)
+				{
+					// No ties: bracket is finished, so just leave:
+					return (new List<MatchModel>());
+				}
+				// else:
+				this.IsFinished = false;
+				List<MatchModel> newMatchModels = new List<MatchModel>();
+
+				// Create a bracket for each group:
+				List<IBracket> tiebreakerBrackets = new List<IBracket>();
+				foreach (List<int> group in tiedGroups)
+				{
+					List<IPlayer> pList = new List<IPlayer>();
+					foreach (int id in group)
+					{
+						pList.Add(Players.Find(p => p.Id == id));
+					}
+					tiebreakerBrackets.Add(new RoundRobinBracket(pList));
+				}
+
+				// "Copy" matches from new pseudo-brackets onto the end of this bracket:
+				int r = 0;
+				while (true) // Run through once for each new round
+				{
+					// Round-by-round, make a list of matches to add:
+					++r;
+					List<IMatch> currRound = new List<IMatch>();
+					foreach (IBracket bracket in tiebreakerBrackets.Where(b => b.NumberOfRounds >= r))
+					{
+						currRound.AddRange(bracket.GetRound(r));
+					}
+					if (0 == currRound.Count)
+					{
+						// No more new matches; break out:
+						break;
+					}
+
+					// Add a new round and copy applicable matches:
+					this.NumberOfRounds++;
+					for (int m = 0; m < currRound.Count; ++m)
+					{
+						Match match = new Match();
+						match.SetMatchNumber(++NumberOfMatches);
+						match.SetRoundIndex(NumberOfRounds);
+						match.SetMatchIndex(m + 1);
+						match.AddPlayer(currRound[m].Players[0]);
+						match.AddPlayer(currRound[m].Players[1]);
+
+						Matches.Add(match.MatchNumber, match);
+						// Also add a model, for firing the event:
+						newMatchModels.Add(GetMatchModel(match));
+					}
+				}
+
+				// Fire event to notify that new Matches were made:
+				OnRoundAdded(new BracketEventArgs(newMatchModels));
+			}
+#endif
 			return (new List<MatchModel>());
 		}
 		protected override List<MatchModel> ApplyGameRemovalEffects(int _matchNumber, List<GameModel> _games, PlayerSlot _formerMatchWinnerSlot)
