b5f4082f78a3ab9b9e8baccf61209f0c3b0dbac0
Tyler Yeary <teyeary@fullsail.edu>
2017-06-12 13:06:12 -0400

    Fixed issue where Players were blank in the tournament.

--- Victorious/WebApplication/Models/Tournament.cs
+++ Victorious/WebApplication/Models/Tournament.cs
@@ -80,23 +80,6 @@ namespace WebApplication.Models
             searched = services.Tournament.FindTournaments(searchData);
         }
 
-        private void ProcessTournament()
-        {
-            if (Model == null)
-            {
-                return;
-            }
-
-            if (TempFixMatchObjects) MatchObjectFix();
-
-            Tourny = new Tournaments.Tournament();
-            Tourny.Title = Model.Title;
-            foreach (BracketModel bracket in Model.Brackets)
-            {
-                Tourny.AddBracket(Tourny.RestoreBracket(bracket));
-            }
-        }
-
         public Tournaments.ITournament GetTournament()
         {
             return Tourny;
@@ -149,24 +132,25 @@ namespace WebApplication.Models
             Model.InProgress = true;
 
             // Update the database
-            CreateMatches(bracket, tourny);
+            bracket.Matches = CreateMatches(bracket, tourny);
             services.Tournament.UpdateBracket(bracket);
             services.Tournament.UpdateTournament(Model);
             return services.Save();
         }
 
-        private void CreateMatches(BracketModel bracketModel, Tournaments.IBracket bracket)
+        private List<MatchModel> CreateMatches(BracketModel bracketModel, Tournaments.IBracket bracket)
         {
-            // Verify if the tournament has not need finalized.
-            if (!bracketModel.Finalized)
+            List<MatchModel> matches = new List<MatchModel>();
+            
+            // Add the matches to the database
+            for (int i = 1; i <= bracket.NumberOfMatches; i++)
             {
-                // Add the matches to the database
-                for (int i = 1; i <= bracket.NumberOfMatches; i++)
-                {
-                    MatchModel matchModel = bracket.GetMatchModel(i);
-                    services.Tournament.AddMatch(matchModel);
-                }
+                matches.Add(bracket.GetMatchModel(i));
+                //bracketModel.Matches.Add(matchModel);
+                //services.Tournament.AddMatch(matchModel);
             }
+
+            return matches;
         }
         #endregion
 
