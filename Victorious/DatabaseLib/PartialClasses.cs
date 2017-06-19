a31ffe37c67e8d33d7c91dc589f73b94eb08152a
Ryan <keltonr01@gmail.com>
2017-06-19 11:44:24 -0400

    Removed default code from OnInit

--- Victorious/DatabaseLib/PartialClasses.cs
+++ Victorious/DatabaseLib/PartialClasses.cs
@@ -43,7 +43,7 @@ namespace DatabaseLib
         public const int LastNameLength = 50;
         public const int UsernameLength = 50;
         public const int EmailLength = 100;
-        public const int PasswordLength = 50;
+        public const int PasswordLength = 255;
 
         partial void OnInit()
         {
@@ -92,10 +92,7 @@ namespace DatabaseLib
     {
         partial void OnInit()
         {
-            WinnerID = -1;
-            CreatedOn = DateTime.Now;
-            LastEditedOn = DateTime.Now;
-
+           
         }
 
     }
