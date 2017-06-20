e6a7caba737acddd87470e35ce518daa718d1e2e
Sean Davis <seandavis@fullsail.edu>
2017-06-20 14:15:07 -0400

    Commenting/Documentation.

--- Victorious/Tournament.Structure/Classes/BracketTypes/SingleElimBracket.cs
+++ Victorious/Tournament.Structure/Classes/BracketTypes/SingleElimBracket.cs
@@ -84,8 +84,17 @@ namespace Tournament.Structure
 		#endregion
 
 		#region Public Methods
+		/// <summary>
+		/// Uses the playerlist to generate the bracket structure & Matches.
+		/// This creates & populates all the Match objects, and ties them together.
+		/// If any Matches already exist, they will be deleted first.
+		/// If there are <2 players, nothing will be made.
+		/// This method is extended in child classes: Double-Elim and GSL brackets.
+		/// </summary>
+		/// <param name="_gamesPerMatch">Max games for every Match</param>
 		public override void CreateBracket(int _gamesPerMatch = 1)
 		{
+			// First, clear any existing Matches and results:
 			ResetBracketData();
 			if (Players.Count < 2)
 			{
@@ -93,11 +102,26 @@ namespace Tournament.Structure
 			}
 
 			#region Create the Bracket
+			/*
+			 * Process: to achieve the correct number of Matches in any Bracket
+			 * (even an abnormally sized one; say, for 11 players),
+			 * we start at the final match. Then we move backward,
+			 * making twice as many Matches for each new round,
+			 * until we hit the correct number of Matches.
+			 * If the first round has a weird number of Matches,
+			 * we'll know our Bracket is abnormally sized and we can deal with it.
+			*/
+
+			// totalMatches is the number of Matches we'll create:
 			int totalMatches = Players.Count - 1;
 			int numMatches = 0;
+			
+			// roundList is how we'll temporarily create & store Matches.
+			// This is a list of rounds (each round is a list of Matches).
+			// Rounds are ordered back-to-front: first is the final match.
 			List<List<Match>> roundList = new List<List<Match>>();
 
-			// Create the Matches
+			// Create the Matches, round-by-round:
 			for (int r = 0; numMatches < totalMatches; ++r)
 			{
 				roundList.Add(new List<Match>());
@@ -105,8 +129,7 @@ namespace Tournament.Structure
 					i < Math.Pow(2, r) && numMatches < totalMatches;
 					++i, ++numMatches)
 				{
-					// Add new matchups per round
-					// (rounds[0] is the final match)
+					// Add new Matches per round:
 					Match m = new Match();
 					m.SetMaxGames(_gamesPerMatch);
 					roundList[r].Add(m);
@@ -128,24 +151,36 @@ namespace Tournament.Structure
 			{
 				if (roundList[r + 1].Count == (roundList[r].Count * 2))
 				{
-					// "Normal" rounds: twice as many matchups
+					// "Normal" rounds: twice as many Matches
+					// (example: 4 Matches -> 8 Matches)
 					for (int m = 0; m < roundList[r].Count; ++m)
 					{
+						// Assign prev/next matchup numbers
 						int currNum = roundList[r][m].MatchNumber;
 
-						// Assign prev/next matchup numbers
+						// [r][3] <-> [r+1][6]
 						roundList[r][m].AddPreviousMatchNumber(roundList[r + 1][m * 2].MatchNumber);
 						roundList[r + 1][m * 2].SetNextMatchNumber(currNum);
 
+						// [r][3] <-> [r+1][7]
 						roundList[r][m].AddPreviousMatchNumber(roundList[r + 1][m * 2 + 1].MatchNumber);
 						roundList[r + 1][m * 2 + 1].SetNextMatchNumber(currNum);
 					}
 				}
-				// Else: round is abnormal. Ignore it for now (we'll handle it later)
+				// Else: round is abnormal. Ignore it for now (we'll handle it later).
 			}
 			#endregion
 
 			#region Assign the Players
+			/*
+			 * In a correctly seeded bracket, high seeds are split apart.
+			 * For instance, the 1 and 2 seeds will each have their own bracket "branch."
+			 * The solution to accomplishing this is to assign seeds
+			 * to the FINAL round. From there, we move them backward one round.
+			 * We fill the Matches in that round (with seeds 3 & 4), then repeat.
+			 * If this is done all the way to the first round, our seeds are correctly split.
+			*/
+
 			// Assign top two seeds to final match
 			int pIndex = 0;
 			roundList[0][0].AddPlayer(Players[pIndex++]);
@@ -153,14 +188,13 @@ namespace Tournament.Structure
 
 			for (int r = 0; r + 1 < roundList.Count; ++r)
 			{
-				// We're shifting back one player for each match in the prev round
+				// We're shifting back one player for each Match in the prev round
 				int prevRoundMatches = roundList[r + 1].Count;
 
 				if ((roundList[r].Count * 2) > prevRoundMatches)
 				{
 					// Abnormal round ahead: we need to allocate prevMatchIndexes
 					// to correctly distribute bye seeds
-
 					int prevMatchNumber = 1;
 
 					for (int m = 0; m < roundList[r].Count; ++m)
@@ -203,25 +237,32 @@ namespace Tournament.Structure
 
 				for (int m = 0; m < roundList[r].Count; ++m)
 				{
-					// For each match, shift/reassign all teams to the prev bracket level
+					// For each match, shift/reassign all Players to the prev bracket level
 					// If prev level is abnormal, only shift 1 (or 0) teams
 					foreach (int n in roundList[r][m].PreviousMatchNumbers)
 					{
 						if (n > 0)
 						{
+							// ReassignPlayers() handles the task of moving Players.
+							// Any Players to be moved from this Match will be removed.
+							// Then, using the match number references,
+							// those Players will be added to the correct Matches.
 							ReassignPlayers(roundList[r][m], roundList[r + 1]);
 							break;
 						}
 					}
 				}
 
+				// Now that the correct Players have been shifted back,
+				// we need to fill in the rest of this round's Player slots
+				// from our master playerlist.
 				for (int prePlayers = pIndex - 1; prePlayers >= 0; --prePlayers)
 				{
 					for (int m = 0; m < prevRoundMatches; ++m)
 					{
 						if (roundList[r + 1][m].Players.Contains(Players[prePlayers]))
 						{
-							// Add prev round's teams (according to seed) from the master list
+							// Add prev round's Players (according to seed) from the master list.
 							roundList[r + 1][m].AddPlayer(Players[pIndex++]);
 							break;
 						}
@@ -231,7 +272,12 @@ namespace Tournament.Structure
 			#endregion
 
 			#region Set Bracket Member Variables
-			// Move bracket data to member variables (Matches dictionary)
+			/* 
+			 * Now we've generated our Bracket.
+			 * Time to move the Matches from the double array
+			 * into an easily accessible Dictionary for storage.
+			 * While we do this, we'll update their data: Round and Match indexes.
+			*/
 			NumberOfRounds = roundList.Count;
 			for (int r = 0; r < roundList.Count; ++r)
 			{
@@ -248,12 +294,26 @@ namespace Tournament.Structure
 		#endregion
 
 		#region Private Methods
+		/// <summary>
+		/// Finds an eliminated player's rank for a single-elim bracket.
+		/// </summary>
+		/// <param name="_matchNumber">Number of finished Match</param>
+		/// <returns>Player's rank</returns>
 		protected override int CalculateRank(int _matchNumber)
 		{
 			int round = GetInternalMatch(_matchNumber).RoundIndex;
 			return Convert.ToInt32(Math.Pow(2, NumberOfRounds - round) + 1);
 		}
 
+		/// <summary>
+		/// Takes the Players from the given Match
+		/// and reassigns them to the correct Matches in the previous round.
+		/// This is used during bracket generation to correctly split up seeds.
+		/// If the Players do not have a previous Match to move to, they remain.
+		/// Otherwise, they are removed from the given Match.
+		/// </summary>
+		/// <param name="_currMatch">Match to move Players out of</param>
+		/// <param name="_prevRound">Previous round: to move Players into</param>
 		private void ReassignPlayers(Match _currMatch, List<Match> _prevRound)
 		{
 			if (null == _currMatch ||
@@ -268,20 +328,26 @@ namespace Tournament.Structure
 			{
 				if (n < 0)
 				{
+					// If a PreviousMatchNumber is -1, that Player will stay here.
 					--playersToMove;
 				}
 			}
 			foreach (Match match in _prevRound)
 			{
+				// Check previous round Matches to find the correct one(s):
 				for (int i = 0; i < _currMatch.PreviousMatchNumbers.Length; ++i)
 				{
 					if (match.MatchNumber == _currMatch.PreviousMatchNumbers[i])
 					{
+						// Add the Player to the found Match:
 						match.AddPlayer(_currMatch.Players[i]);
+						// Remove the Player from the given Match:
 						_currMatch.RemovePlayer(_currMatch.Players[i].Id);
+
 						--playersToMove;
 					}
 				}
+
 				if (0 == playersToMove)
 				{
 					break;
