21e0d3837f27de503e773cf02a856c641561d58f
Tyler Yeary <teyeary@fullsail.edu>
2017-07-09 00:08:53 -0400

    Added MySQL Entity

--- Victorious/DatabaseLib/UnitOfWork.cs
+++ Victorious/DatabaseLib/UnitOfWork.cs
@@ -1,12 +1,4 @@
-﻿using DatabaseLib;
-using System;
-using System.Collections.Generic;
-using System.Data.Entity.Core.Objects;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data.Entity.Utilities;
-using System.Data.Entity.Validation;
+﻿using System;
 
 namespace DatabaseLib
 {
