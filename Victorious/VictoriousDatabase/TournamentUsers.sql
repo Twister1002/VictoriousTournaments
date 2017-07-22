1c2b26ac1dec54479524d928f90b51d2e0b8f55f
Tyler Yeary <teyeary@fullsail.edu>
2017-07-22 17:49:17 -0400

    Fixed issue where bit was not defaulted correctly.

--- Victorious/VictoriousDatabase/TournamentUsers.sql
+++ Victorious/VictoriousDatabase/TournamentUsers.sql
@@ -7,7 +7,7 @@
     [PermissionLevel] INT NULL, 
     [Name] NVARCHAR(50) NOT NULL, 
     [InviteCode] NVARCHAR(255) NULL, 
-    [IsCheckedIn] BIT NULL, 
+    [IsCheckedIn] BIT NOT NULL DEFAULT 0, 
     [CheckInTime] DATETIME NULL, 
     CONSTRAINT [PK_TournamentUsers] PRIMARY KEY ([TournamentUserID]),
 	CONSTRAINT FK_TournamentUser_Tournaments FOREIGN KEY(TournamentID) REFERENCES Tournaments(TournamentID)
