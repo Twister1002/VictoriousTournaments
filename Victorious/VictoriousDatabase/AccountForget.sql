184d17aeef51cfc9b3789ea760aa86c4e264a322
Tyler Yeary <teyeary@fullsail.edu>
2017-08-10 21:36:39 -0400

    Created new table in database project

---
+++ Victorious/VictoriousDatabase/AccountForget.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[AccountForget]
+(
+	[AccountForgetID] INT NOT NULL PRIMARY KEY, 
+    [AccountID] INT NOT NULL, 
+    [Token] VARCHAR(100) NOT NULL, 
+    [DateIssued] DATETIME NOT NULL, 
+    [DateUsed] DATETIME NULL, 
+    CONSTRAINT [FK_AccountForget_ToTable_AccountModel] FOREIGN KEY (AccountID) REFERENCES Accounts(AccountID)
+)
